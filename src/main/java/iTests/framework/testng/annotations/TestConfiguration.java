dd2eababf81ccdf48d6424b95185fc11b92a8b40
nirbir <nirb@gigaspaces.com>
2013-05-08 15:33:27 +0300

    change retention policy

--- src/main/java/iTests/framework/testng/annotations/TestConfiguration.java
+++ src/main/java/iTests/framework/testng/annotations/TestConfiguration.java
@@ -3,7 +3,7 @@ package iTests.framework.testng.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 public @interface TestConfiguration {
 
     public enum VM {
