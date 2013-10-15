3ac0bc653c31c6c07277d1920c3a4be8d2f11b82
kobi <kobkis@gmail.com>
2013-10-15 17:04:22 +0300

    Wrap cause field with CDATA - increased version

--- src/main/java/iTests/framework/testng/report/TestsReportFileStream.java
+++ src/main/java/iTests/framework/testng/report/TestsReportFileStream.java
@@ -1,6 +1,7 @@
 package iTests.framework.testng.report;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import iTests.framework.testng.report.xml.CustomizedDomDriver;
 import iTests.framework.testng.report.xml.TestsReport;
 
@@ -67,7 +68,7 @@ public class TestsReportFileStream {
 		FileReader fileReader = null;
 		try {
 			fileReader = new FileReader(file);
-			XStream xStream = new XStream(new CustomizedDomDriver());
+			XStream xStream = new XStream(new DomDriver());
 			Object obj = xStream.fromXML(fileReader);
 			TestsReport testsReport = (TestsReport)obj;
 			return testsReport;
