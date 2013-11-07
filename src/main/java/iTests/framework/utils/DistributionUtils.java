c584f6a8660d4d7e7a2578f97d99b926a79c4cc3
kobi <kobkis@gmail.com>
2013-11-07 08:19:56 +0200

    Fixed method - assert even primaries distribution in the same pu only

--- src/main/java/iTests/framework/utils/DistributionUtils.java
+++ src/main/java/iTests/framework/utils/DistributionUtils.java
@@ -41,42 +41,41 @@ public class DistributionUtils {
 	/**
 	 * assert that primaries are evenly distributed across machines
 	 */
-	public static void assertEvenlyDistributedPrimaries(Admin admin) {
-		int nPrimaries = 0;
-		ProcessingUnit[] processingUnits = admin.getProcessingUnits().getProcessingUnits();
-		for (ProcessingUnit pu : processingUnits) {
-			nPrimaries += pu.getPartitions().length;
-		}
-		
-		/*
-		 * since there can be more machines which are not participating in the test,
-		 * count only the machines with GSCs
-		 */
-		int nMachines = 0;
-		for (Machine machine : admin.getMachines()) {
-			if (!machine.getGridServiceContainers().isEmpty()) {
-				++nMachines;
-			}
-		}
+    public static void assertEvenlyDistributedPrimaries(Admin admin) {
+        for(ProcessingUnit pu : admin.getProcessingUnits().getProcessingUnits()){
+            int nPrimaries = 0;
+            nPrimaries += pu.getPartitions().length;
 
-		int nPrimariesPerMachine = (int) Math.ceil(1.0 * nPrimaries / nMachines);
-		
-		for (Machine machine : admin.getMachines()) {
-			int nCurrentPrimariesInMachine = 0;
-			ProcessingUnitInstance[] processingUnitInstances = machine.getProcessingUnitInstances();
-			for (ProcessingUnitInstance puInstance : processingUnitInstances) {
-                puInstance.waitForSpaceInstance();
-				if (puInstance.getSpaceInstance().getMode().equals(SpaceMode.PRIMARY)) {
-					++nCurrentPrimariesInMachine;
-				}
-			}
-			
-			assertTrue("Non evenly distributed primaries, expected: maximum "
-					+ nPrimariesPerMachine + " per machine, actual: "
-					+ nCurrentPrimariesInMachine,
-					nCurrentPrimariesInMachine <= nPrimariesPerMachine);
-		}
-	}
+            /*
+		    * since there can be more machines which are not participating in the test,
+		    * count only the machines with GSCs
+		    */
+            int nMachines = 0;
+            for (Machine machine : admin.getMachines()) {
+                if (!machine.getGridServiceContainers().isEmpty()) {
+                    ++nMachines;
+                }
+            }
+
+            int nPrimariesPerMachine = (int) Math.ceil(1.0 * nPrimaries / nMachines);
+
+            for (Machine machine : admin.getMachines()) {
+                int nCurrentPrimariesInMachine = 0;
+                ProcessingUnitInstance[] processingUnitInstances = machine.getProcessingUnitInstances();
+                for (ProcessingUnitInstance puInstance : processingUnitInstances) {
+                    puInstance.waitForSpaceInstance();
+                    if (puInstance.getSpaceInstance().getMode().equals(SpaceMode.PRIMARY)) {
+                        ++nCurrentPrimariesInMachine;
+                    }
+                }
+
+                assertTrue("Non evenly distributed primaries, expected: maximum "
+                        + nPrimariesPerMachine + " per machine, actual: "
+                        + nCurrentPrimariesInMachine,
+                        nCurrentPrimariesInMachine <= nPrimariesPerMachine);
+            }
+        }
+    }
 	
 	/**
 	 * assert that the number of primaries matches the expected
