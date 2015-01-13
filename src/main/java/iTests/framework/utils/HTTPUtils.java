44943e5fe91d969e3bae06b44ac511ad33d3a6bb
yohana <yohaha@yohanakhoury.com>
2015-01-13 23:49:00 +0200

    GS-12126 Added method for performing PUT requests

--- src/main/java/iTests/framework/utils/HTTPUtils.java
+++ src/main/java/iTests/framework/utils/HTTPUtils.java
@@ -5,9 +5,12 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
@@ -79,19 +82,63 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
     public static class HTTPSession {
         BasicCookieStore cookieStore = new BasicCookieStore();
 
-        public HashMap<PARAMS, String> post(HTTPPostRequest postRequest) throws IOException {
-            HashMap<PARAMS, String> result = postRequest.post(cookieStore);
-            return result;
-        }
-        public HashMap<PARAMS, String> get(HTTPGetRequest getRequest) throws IOException {
-            HashMap<PARAMS, String> result = getRequest.get(cookieStore);
+        public HTTPResponse post(HTTPPostRequest postRequest) throws IOException {
+            HTTPResponse result = postRequest.post(cookieStore);
             return result;
         }
 
+        public HTTPResponse put(HTTPPutRequest putRequest) throws IOException {
+            HTTPResponse result = putRequest.put(cookieStore);
+            return result;
+        }
+
+        public HTTPResponse get(HTTPGetRequest getRequest) throws IOException {
+            HTTPResponse result = getRequest.get(cookieStore);
+            return result;
+        }
+        public HTTPResponse delete(HTTPDeleteRequest deleteRequest) throws IOException {
+            return deleteRequest.get(cookieStore);
+        }
+
     }
+
+    public static class HTTPResponse {
+        private HashMap<PARAMS, String> response;
+        protected HTTPResponse() {
+            response = new HashMap<PARAMS, String>();
+        }
+
+        public int getStatusCode() {
+            return Integer.valueOf(response.get(PARAMS.RESPONSE_CODE));
+        }
+
+        public String getBody() {
+            return response.get(PARAMS.RESPONSE_BODY);
+        }
+
+        public String getCookies() {
+            return response.get(PARAMS.COOKIES);
+        }
+
+        public String get(PARAMS param) {
+            return response.get(param);
+        }
+
+        public void set(PARAMS param, String value) {
+            response.put(param, value);
+        }
+
+        @Override
+        public String toString() {
+            return response.toString();
+        }
+    }
+
+
     public static class HTTPPostRequest {
         private final String _urlAsString;
         private final List<BasicNameValuePair> postParams;
+        private String jsonBody;
 
         public HTTPPostRequest (String urlAsString) {
             _urlAsString = urlAsString;
@@ -103,11 +150,21 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
             return this;
         }
 
-        protected HashMap<PARAMS, String> post(CookieStore cookieStore) throws IOException {
+        public HTTPPostRequest withJSONBody(String jsonBody) {
+            this.jsonBody = jsonBody;
+            return this;
+        }
+
+        protected HTTPResponse post(CookieStore cookieStore) throws IOException {
             DefaultHttpClient httpClient = new DefaultHttpClient();
             httpClient.setCookieStore(cookieStore);
             HttpPost httpPost = new HttpPost(_urlAsString);
-            httpPost.setEntity(new UrlEncodedFormEntity(postParams, "UTF-8"));
+            if (jsonBody != null) {
+                httpPost.setEntity(new StringEntity(jsonBody, "UTF-8"));
+                httpPost.setHeader("Content-type", "application/json; charset=UTF-8");
+            } else {
+                httpPost.setEntity(new UrlEncodedFormEntity(postParams, "UTF-8"));
+            }
 
             httpClient.setRedirectStrategy(new DefaultRedirectStrategy());
             httpPost.getParams().setParameter("http.protocol.handle-redirects",true);
@@ -116,9 +173,9 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
 
             HttpEntity entity = response.getEntity();
             String responseString = EntityUtils.toString(entity, "UTF-8");
-            HashMap<PARAMS, String> result = new HashMap<PARAMS, String>();
-            result.put(PARAMS.RESPONSE_BODY, responseString);
-            result.put(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
+            HTTPResponse result = new HTTPResponse();
+            result.set(PARAMS.RESPONSE_BODY, responseString);
+            result.set(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
             Iterator<Cookie> it = cookieStore.getCookies().iterator();
             String cookies ="";
             while (it.hasNext()) {
@@ -128,7 +185,7 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
                     cookies += ";";
                 }
             }
-            result.put(PARAMS.COOKIES, cookies);
+            result.set(PARAMS.COOKIES, cookies);
             return result;
         }
     }
@@ -140,7 +197,7 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
             _urlAsString = urlAsString;
         }
 
-        protected HashMap<PARAMS, String> get(CookieStore cookieStore) throws IOException {
+        protected HTTPResponse get(CookieStore cookieStore) throws IOException {
             DefaultHttpClient httpClient = new DefaultHttpClient();
             httpClient.setCookieStore(cookieStore);
             HttpGet httpGet = new HttpGet(_urlAsString);
@@ -148,9 +205,9 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
             HttpResponse response = httpClient.execute(httpGet);
             HttpEntity entity = response.getEntity();
             String responseString = EntityUtils.toString(entity, "UTF-8");
-            HashMap<PARAMS, String> result = new HashMap<PARAMS, String>();
-            result.put(PARAMS.RESPONSE_BODY, responseString);
-            result.put(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
+            HTTPResponse result = new HTTPResponse();
+            result.set(PARAMS.RESPONSE_BODY, responseString);
+            result.set(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
             Iterator<Cookie> it = cookieStore.getCookies().iterator();
             String cookies ="";
      /*       String urlString = _urlAsString;
@@ -167,9 +224,103 @@ Commented by Yohana. Reason: Not tested thus might not work as expected!
                     }
                 }
             }
-            result.put(PARAMS.COOKIES, cookies);
+            result.set(PARAMS.COOKIES, cookies);
             return result;
         }
     }
 
+
+    public static class HTTPDeleteRequest {
+        private final String _urlAsString;
+
+        public HTTPDeleteRequest (String urlAsString) {
+            _urlAsString = urlAsString;
+        }
+
+        protected HTTPResponse get(CookieStore cookieStore) throws IOException {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            httpClient.setCookieStore(cookieStore);
+            HttpDelete httpGet = new HttpDelete(_urlAsString);
+
+            HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            HTTPResponse result = new HTTPResponse();
+            result.set(PARAMS.RESPONSE_BODY, responseString);
+            result.set(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
+            Iterator<Cookie> it = cookieStore.getCookies().iterator();
+            String cookies ="";
+     /*       String urlString = _urlAsString;
+            if (urlString.lastIndexOf('/') == urlString.length()-1) {
+                urlString = urlString.substring(0, urlString.length() -1);
+            }*/
+            URL url = new URL(/*urlString*/_urlAsString);
+            while (it.hasNext()) {
+                Cookie c = it.next();
+                if (c.getDomain().equals(url.getHost()) /*&& c.getPath().equals(url.getPath())*/) {
+                    cookies += c.getName() + "=" + c.getValue();
+                    if (it.hasNext()) {
+                        cookies += ";";
+                    }
+                }
+            }
+            result.set(PARAMS.COOKIES, cookies);
+            return result;
+        }
+    }
+
+    public static class HTTPPutRequest {
+        private final String _urlAsString;
+        private final List<BasicNameValuePair> postParams;
+        private String jsonBody;
+
+        public HTTPPutRequest (String urlAsString) {
+            _urlAsString = urlAsString;
+            postParams = new ArrayList<BasicNameValuePair>();
+        }
+
+        public HTTPPutRequest withParameter(String key, String value) {
+            postParams.add(new BasicNameValuePair(key, value));
+            return this;
+        }
+
+        public HTTPPutRequest withJSONBody(String jsonBody) {
+            this.jsonBody = jsonBody;
+            return this;
+        }
+
+        protected HTTPResponse put(CookieStore cookieStore) throws IOException {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            httpClient.setCookieStore(cookieStore);
+            HttpPut httpPut = new HttpPut(_urlAsString);
+            if (jsonBody != null) {
+                httpPut.setEntity(new StringEntity(jsonBody, "UTF-8"));
+                httpPut.setHeader("Content-type", "application/json; charset=UTF-8");
+            } else {
+                httpPut.setEntity(new UrlEncodedFormEntity(postParams, "UTF-8"));
+            }
+
+            httpClient.setRedirectStrategy(new DefaultRedirectStrategy());
+            httpPut.getParams().setParameter("http.protocol.handle-redirects",true);
+
+            HttpResponse response = httpClient.execute(httpPut);
+
+            HttpEntity entity = response.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            HTTPResponse result = new HTTPResponse();
+            result.set(PARAMS.RESPONSE_BODY, responseString);
+            result.set(PARAMS.RESPONSE_CODE, String.valueOf(response.getStatusLine().getStatusCode()));
+            Iterator<Cookie> it = cookieStore.getCookies().iterator();
+            String cookies ="";
+            while (it.hasNext()) {
+                Cookie c = it.next();
+                cookies += c.getName()+"="+c.getValue();
+                if (it.hasNext()) {
+                    cookies += ";";
+                }
+            }
+            result.set(PARAMS.COOKIES, cookies);
+            return result;
+        }
+    }
 }
